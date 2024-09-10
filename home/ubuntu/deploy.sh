d4bf237186f465eba77123d5a70ebe7d3804c885
lpm0073 <lpm0073@gmail.com>
2024-09-10 15:56:13 -0600

    fix: pull new image before starting deployment

--- home/ubuntu/deploy.sh
+++ home/ubuntu/deploy.sh
@@ -1,6 +1,7 @@
 
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 194722410770.dkr.ecr.ap-south-1.amazonaws.com
+docker pull 194722410770.dkr.ecr.ap-south-1.amazonaws.com/ilmcamp/openedx-v18-prod:latest
 tutor config save --set LANGUAGE_CODE="uz"
 tutor local launch
 tutor local do settheme ilmcamp-openedx-theme
